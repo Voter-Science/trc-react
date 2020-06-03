bc73bcad857d686ef2a020cc82d778f45dcae578
dinohamzic <dino@subtlebits.com>
2020-06-03 19:16:08 +0200

    Create Grid component

---
+++ src/Grid.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+// Grid is a generic grid layout component.
+
+interface IProps {
+  children: React.ReactNode[];
+}
+
+const Container = styled.div`
+  display: grid;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+  margin: 1rem 0;
+`;
+
+export const Grid = ({ children }: IProps) => (
+  <Container>
+    {children.map((child, i) => (
+      <div key={i}>
+        {child}
+      </div>
+    ))}
+  </Container>
+);
