7f7bab9063e005333f8475797ad1d9439e74f6b9
dinohamzic <dino@subtlebits.com>
2020-12-21 18:39:25 +0100

    Improve disabled button style

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -1,16 +1,19 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  secondary?: boolean
+  secondary?: boolean;
 }
 
 export const Button = styled.button<IProps>`
-    color: #fff;
-    background: ${props => props.secondary ? '#777' : '#6485ff'};
-    border: none;
-    border-radius: 2px;
-    cursor: pointer;
-    font-size: .9rem;
-    height: 2.4rem;
-    padding: 0 1rem;
+  color: #fff;
+  background: ${(props) => (props.secondary ? "#777" : "#6485ff")};
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+  font-size: 0.9rem;
+  height: 2.4rem;
+  padding: 0 1rem;
+  &:disabled {
+    opacity: 0.5;
+  }
 `;
