c5ef79970e8f4823624597f362887b7d01bc6ae9
dinohamzic <dino@subtlebits.com>
2021-01-13 18:37:32 +0100

    Make CSV component reactive

--- src/DownloadCsv.tsx
+++ src/DownloadCsv.tsx
@@ -5,60 +5,57 @@ import { SheetContents, ISheetContents } from "trc-sheet/sheetContents";
 // Generic download button for a CSV
 
 interface IProps {
-    data: ISheetContents;
+  data: ISheetContents;
 }
 
 export class DownloadCsv extends React.Component<IProps, {}> {
-    private _csv :string;
-    private _error : string;
+  private _csv: string;
+  private _error: string;
 
-    constructor(props: any) {
-        super(props);
+  constructor(props: any) {
+    super(props);
 
-        this.onClickDownload1 = this.onClickDownload1.bind(this);
+    this.onClickDownload1 = this.onClickDownload1.bind(this);
 
-        try {
-            this._csv = SheetContents.toCsv(this.props.data);
-        } catch (err) {
-            this._error = err;
-        }
+    try {
+      this._csv = SheetContents.toCsv(this.props.data);
+    } catch (err) {
+      this._error = err;
+    }
+  }
+
+  private onClickDownload1(): void {
+    var content = this._csv;
+    window.navigator.msSaveBlob(
+      new Blob([content], { type: "text/csv;charset=utf-8;" }),
+      "data.csv"
+    );
+  }
+
+  public render() {
+    this._csv = SheetContents.toCsv(this.props.data);
+
+    if (this._error) {
+      return <div>Error: {this._error}</div>;
     }
 
-    private onClickDownload1() : void {
-        var content = this._csv;
-        window.navigator.msSaveBlob(
-            new Blob([content],
-            { type: "text/csv;charset=utf-8;" }),
-            "data.csv"
-        );
+    if (!this._csv) {
+      return null;
     }
 
-    public render()  {
-        if (this._error) {
-            return <div>Error: {this._error}</div>
-        }
+    var img =
+      "https://trcanvasdata.blob.core.windows.net/publicimages/export-csv.png";
 
-        if (!this._csv) {
-            return null;
-        }
+    if (window.navigator.msSaveBlob) {
+      return <input type="image" src={img} onClick={this.onClickDownload1} />;
+    } else {
+      let uri = "data:text/csv;charset=utf-8," + encodeURIComponent(this._csv);
 
-        var img = "https://trcanvasdata.blob.core.windows.net/publicimages/export-csv.png";
-
-        if (window.navigator.msSaveBlob) {
-            return <input type="image" src={img} onClick={this.onClickDownload1} />
-        }
-
-        else {
-            let uri = "data:text/csv;charset=utf-8," + encodeURIComponent(this._csv);
-
-            return (
-                <a
-                    href={uri}
-                    download="data.csv"
-                >
-                    <img src={img} />
-                </a>
-            );
-        }
+      return (
+        <a href={uri} download="data.csv">
+          <img src={img} />
+        </a>
+      );
     }
+  }
 }
